708472777e9a36f9db5dde518349ae60ea95e79e
kevin <kiariekevin22@gmail.com>
2020-12-08 05:29:14 +0300

    model viewset to replace create,update,delete and list views

--- backend/src/djreact/articles/models.py
+++ backend/src/djreact/articles/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
